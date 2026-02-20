5a7365760e82ea949d182a2ffe9100a4e7d56c23
Anand302004 <ananddeshmane69@gmail.com>
2026-02-20 13:32:05 +0530

    database url connection

--- db.js
+++ db.js
@@ -1,15 +1,11 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-import dotenv from "dotenv";
-dotenv.config();
-
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
